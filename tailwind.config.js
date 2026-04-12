b32e12d1828a47381e68247a662bf6811394cf29
Gold Bassey Edem <everydayhealthafrica@gmail.com>
2026-04-12 02:35:26 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,18 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        // 'display' for bold, impact headers, 'sans' for clean body text
+        display: ['Oswald', 'sans-serif'], 
+        sans:['Inter', 'sans-serif'],
+      },
       colors: {
         emeraldGreen: '#008000',
         emeraldYellow: '#FFD700',
+        // Adding premium dark theme colors
+        darkBase: '#0a0a0a', // Almost black background
+        darkSurface: '#141414', // Slightly lighter for cards
+        darkBorder: '#2a2a2a', // Thin borders for cards
       }
     },
   },
